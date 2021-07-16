fa9a6e3691578488e20e9b01e0e0ca36dbd9e01b
viacheslavkrupnov <slava86krupnov@gmail.com>
2021-07-17 01:20:31 +0300

    made tasks

--- src/App.js
+++ src/App.js
@@ -1,3 +1,60 @@
-export default function App() {
-  return <h1>Hello World</h1>;
+import { Component } from 'react';
+
+import Container from './components/Container';
+import Section from './components/Section';
+import FeedbackOptions from './components/FeedbackOptions';
+import Statistics from './components/Statistics';
+import Notification from './components/Notification';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  handelFeedbeak = option => {
+    this.setState(prefState => ({
+      [option]: prefState[option] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((acc, option) => acc + option, 0);
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    return Math.round((good / this.countTotalFeedback()) * 100);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positiveFeedback = this.countPositiveFeedbackPercentage();
+
+    return (
+      <Container>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.handelFeedbeak}
+          />
+        </Section>
+        <Section title="Statistics">
+          {total === 0 ? (
+            <Notification message="No feedback given" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positiveFeedback={positiveFeedback}
+            />
+          )}
+        </Section>
+      </Container>
+    );
+  }
 }
+export default App;
